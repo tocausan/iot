1d13dfcb5981e3e12947aa18e46f2d5860521df4
tocausan <tocausan@gmail.com>
2017-02-18 21:42:09 +0100

    promise

--- server-dashboard/service/public/scripts/mainController.js
+++ server-dashboard/service/public/scripts/mainController.js
@@ -3,15 +3,38 @@
 angular.module('mainController', [])
     .controller('mainController', ($scope) => {
 
+        // http get
+        const httpget = (url) => {
+            new Promise(
+                (resolve, reject) => {
+                    const req = new XMLHttpRequest();
+                    // get page
+                    req.open('GET', url);
+                    // onload status
+                    req.onload = () => {
+                        if (req.status == 200) {
+                            resolve(req.response);
+                        } else {
+                            reject(req.statusText);
+                        };
+                    };
+                    // error handler
+                    req.onerror = () => {
+                        reject(Error('Network error'));
+                    };
+                    // send request 
+                    req.send();
+                }
+            );
+        }
+
         // restart the sevrer
         $scope.shutdownServer = () => {
-            document.location.href = "server/shutdown";
-            document.location.href = "/";
+            return httpget('server/shutdown');
         }
 
         // shutdown the server
         $scope.rebootServer = () => {
-            document.location.href = "server/reboot";
-            document.location.href = "/";
+            return httpget('server/reboot');
         }
     })
